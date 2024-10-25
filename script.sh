59db6a7f8aeb3480fc2be78de6696f8887450306
Jugal Kishore <me@devjugal.com>
2024-10-25 16:57:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -101,6 +101,7 @@ else
 	git add .
 	git commit -m "CI Build"
 	if [[ -n ${GITLAB_TOKEN-} ]]; then
+ 		git remote remove origin
 		git remote add origin "https://oauth2:${GITLAB_TOKEN}@${GL_REF}"
 		git push origin HEAD:master
 		echo -e "${GREEN}Clang Toolchain Pushed${NC}"
